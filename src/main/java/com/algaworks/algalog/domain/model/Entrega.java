b899ffdde795e1a2e08f5f06691677bf1dd0ef5a
José Ronaldo <dev.joseronaldo@gmail.com>
2023-03-23 11:24:06 -0300

    feat: validations and order routes

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -1,13 +1,19 @@
 package com.algaworks.algalog.domain.model;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Getter
 @Setter
@@ -20,11 +26,18 @@ public class Entrega {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Valid
+    @ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+    @NotNull
     @ManyToOne
     private Cliente cliente;
 
+    @Valid
+    @NotNull
     @Embedded
     private Destinatario destinatario;
+
+    @NotNull
     private BigDecimal taxa;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
@@ -32,8 +45,8 @@ public class Entrega {
     private StatusEntrega status;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private LocalDateTime dataPedido;
+    private OffsetDateTime dataPedido;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private LocalDateTime dataFinalizacao;
+    private OffsetDateTime dataFinalizacao;
 }
